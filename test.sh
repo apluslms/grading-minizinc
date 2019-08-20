23ee1209f12384ecb9b91f300ffa9f3fa9263bc9
Tommi Junttila <Tommi.Junttila@aalto.fi>
2019-08-20 12:43:20 +0300

    Update grading-base to grading-python and the minizinc version

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ echo " -------- Building test image -------------"
 docker build -t apluslms/grading-minizinc:test .
 
 echo " -------- Executing test image ------------"
-if docker run --rm -it -v `pwd`:/submission apluslms/grading-minizinc:test mzn-gecode test.mzn; then
+if docker run --rm -it -v `pwd`:/submission apluslms/grading-minizinc:test minizinc --solver org.gecode.gecode test.mzn; then
     echo " -- OK!"
 else
     echo " !!! ERROR !!! "
